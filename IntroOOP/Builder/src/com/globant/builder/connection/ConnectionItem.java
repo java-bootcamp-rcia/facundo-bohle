532fe141d566147e579635a19c2fd33f2b185db7
bohledevs <facubohle2@gmail.com>
2019-12-03 16:08:58 -0300

    Refactorized the method getValue() for connection items in  Builder project

--- IntroOOP/Builder/src/com/globant/builder/connection/ConnectionItem.java
+++ IntroOOP/Builder/src/com/globant/builder/connection/ConnectionItem.java
@@ -1,11 +1,20 @@
 package com.globant.builder.connection;
 
+import java.util.Scanner;
+
 public abstract class ConnectionItem {
 
   // Connection item field
   protected String value="";
 
   // Retrieves info
-  public abstract String getValue();
+  public String getValue() {
+    if (this.value.equals("")) {
+      Scanner input = new Scanner(System.in);
 
+      System.out.println("Enter a valid"+this.getClass().getName());
+      this.value = input.nextLine();
+    }
+    return this.value;
+  }
 }
--- IntroOOP/Builder/src/com/globant/builder/connection/URL.java
+++ IntroOOP/Builder/src/com/globant/builder/connection/URL.java
@@ -4,15 +4,7 @@ import java.util.Scanner;
 
 public class URL extends ConnectionItem {
 
-  @Override
-  public String getValue() {
-    if (this.value.equals("")) {
-      Scanner input = new Scanner(System.in);
-      System.out.println("Enter a valid URL:");
-      this.value = input.nextLine();
-    }
-    return this.value;
-  }
+
 
 
 }
